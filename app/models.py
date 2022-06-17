c7238dfd40b138b418692a708ba9e4ab1b698f00
Ignacio Diaz <ign.diaz@duocuc.cl>
2022-06-16 22:27:58 -0400

    se agrego el modelo de usuario

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    rut = models.CharField(primary_key=True, max_length=14)
+    nombre = models.CharField(max_length=16, null=False)
+    apellido = models.CharField(max_length=16, null=False)
+    telefono = models.IntegerField(null=False)
+    email = models.EmailField(null=False)
+    pwd = models.CharField(null=False, max_length=12)

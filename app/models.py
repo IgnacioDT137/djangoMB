ec5dc54d83d472e2ec3b49066b180f3c7a559514
jhonavelasquez <jon.velasquez@duocuc.cl>
2022-06-18 21:10:54 -0400

    se agregó el modelo de producto

--- app/models.py
+++ app/models.py
@@ -9,3 +9,11 @@ class Usuario(models.Model):
     telefono = models.IntegerField(null=False)
     email = models.EmailField(null=False, unique=True)
     pwd = models.CharField(null=False, max_length=12)
+
+class Producto(models.Model):
+    codigo = models.IntegerField(primary_key=True, max_length=6)
+    nombre = models.CharField(max_length=30)
+    marca = models.CharField(max_length=20)
+    precio = models.IntegerField(max_length=20)
+    stock = models.IntegerField(max_length=20)
+    imagen = models.CharField(max_length=500, default= "imagen")

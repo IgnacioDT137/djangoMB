39a181b3644fe9587fd96dd950d2e2543af0a30a
Martin3125 <101206120+Martin3125@users.noreply.github.com>
2022-06-18 23:16:33 -0400

    se modifico la url de registro y se agrego la de login

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', registro, name='registro'),
+    path('',login, name='login'),
+    path('registro', registro, name='registro'),
     path('tienda', tienda, name="tienda"),  
 ]

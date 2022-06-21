4251deba08ddda9c51f090869fd567f1fa5956d2
Martin3125 <101206120+Martin3125@users.noreply.github.com>
2022-06-20 23:50:45 -0400

    se agrego la clase  reserva de mantencion

--- app/models.py
+++ app/models.py
@@ -17,3 +17,11 @@ class Producto(models.Model):
     precio = models.IntegerField(max_length=20)
     stock = models.IntegerField(max_length=20)
     imagen = models.CharField(max_length=500, default= "imagen")
+
+class Reserva_mantencion(models.Model):
+    id_mantencion = models.AutoField(primary_key=True)
+    usuario = models.EmailField(null=False)
+    tipo_bici = models.CharField(max_length=30)
+    tipo_mantencio = models.CharField(max_length=30)
+    detalles = models.CharField(max_length=1000)
+
